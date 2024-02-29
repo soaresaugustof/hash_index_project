aecf604bb0d03aa10f46c997e8376513ca0694d5
samueo <96780102+szaire@users.noreply.github.com>
2024-02-29 18:56:14 -0300

    :pushpin: 1855_290224: extra commit

--- backend/Controllers/HashController.cs
+++ backend/Controllers/HashController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,11 +9,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]    
+    [Route("api/[controller]")]
     public class HashController : ControllerBase
     {
+        private readonly string projectPath = AppDomain.CurrentDomain.BaseDirectory;
+
         // InitBook
         [HttpPost]
-        
+        public void InitBook()
+        {
+            Console.WriteLine(projectPath);
+
+            // string[] lines;
+            // var list = new List<string>();
+            // var fileStream = new FileStream(
+            //     $"{projectPath}\\words.txt",
+            //     FileMode.Open,
+            //     FileAccess.Read
+            // );
+        }
     }
 }
