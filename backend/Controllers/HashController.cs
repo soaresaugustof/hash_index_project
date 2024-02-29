564f27c11659cba23409fbbfffde4d417e764184
samueo <96780102+szaire@users.noreply.github.com>
2024-02-29 18:18:10 -0300

    :pushpin: 1817_290224: add .gitignore

---
+++ backend/Controllers/HashController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    public class HashController : ControllerBase
+    {
+        private Page
+    }
+}
