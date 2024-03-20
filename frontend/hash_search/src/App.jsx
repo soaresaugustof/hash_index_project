780915998a64f342841d83d59a62cae9e8f1280b
soaresaugustof <soaresaugusto@edu.unifor.br>
2024-03-20 17:22:10 -0300

    frontend e começo de requisições

--- frontend/hash_search/src/App.jsx
+++ frontend/hash_search/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dashboard from './components/Dasboard';
 import useSWR from 'swr';
+import axios,{ AxiosRequestConfig } from 'axios';
 
 const fetcher = (url, quantidade) => fetch(`${url}?quantidade=${quantidade}`).then((res) => res.json());
 
 function App() {
   const [quantidade, setQuantidade] = useState(10);
+  const [tuplas, setTuplas] = useState(10);
   const [chave, setChave] = useState('');
   const { data: resultados, error } = useSWR(['URL_DA_SUA_API', quantidade, chave], fetcher);
 
@@ -27,21 +29,71 @@ function App() {
      */
   }
 
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('url-do-backend', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Erro ao fazer requisição: ', error);
+        }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <img src="./img/hashr.svg" alt="Logo" />
       <form onSubmit={(e) => { e.preventDefault(); }}>
-        <label>
-          Quantidade de Registros:
-          <input
-            type="number"
-            value={quantidade}
-            onChange={(e) => setQuantidade(e.target.value)}
-          />
-        </label>
-        <button type="submit">Buscar</button>
+        <header>
+            <nav>
+                <h1>#ASH READER</h1>
+            </nav>
+        </header>
+        <main className="main">
+            <div className="title">
+                <h2>Campos de busca</h2>
+            </div>
+            <section className="pesquisa">
+                <div className="searchArrange">
+                    <div>
+                        <p>Chave de busca:</p>
+                        <input type="text" value={chave} onChange={(e) => setChave(e.target.value)}/>
+                    </div>
+                    <div>
+                        <p>Número de registros por página:</p>
+                        <input type="number" value={quantidade} onChange={(e) => setQuantidade(e.target.value)} />
+                    </div>
+                    <div>
+                        <p>Número de tuplas para scan:</p>
+                        <input type="number" value={tuplas} onChange={(e) => setTuplas(e.target.value)} />
+                    </div>
+                </div>
+                <div className="searchArrange">
+                    <button type="submit">Pesquisar</button>
+                </div>
+            </section>
+            <section className="infos">
+                <p>Taxa de colisão:</p>
+                <p>Taxa de overflow:</p>
+                <p>Estimativa de custo:</p>
+            </section>
+            <div className="resultLabel">
+                <h2>Tela de resultados</h2>
+            </div>
+            <section className="dashboard">
+                <Dashboard resultados={resultados} />
+            </section>
+        </main>
       </form>
-      <Dashboard  resultados={resultados} />
     </div>
   );
 }
