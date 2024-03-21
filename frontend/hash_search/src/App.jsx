f5b324ecd9e162da8539590085ce1ea8d3bfe55f
Paulo Capelo <paulodcsc@gmail.com>
2024-03-20 23:59:32 -0300

    :rocket: - Submit e botões adaptados para os 3 casos de busca

--- frontend/hash_search/src/App.jsx
+++ frontend/hash_search/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Dashboard from './components/Dasboard';
 import useSWR from 'swr';
-import axios,{ AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
 const fetcher = (url, quantidade) => fetch(`${url}?quantidade=${quantidade}`).then((res) => res.json());
 
@@ -10,7 +10,9 @@ function App() {
   const [quantidade, setQuantidade] = useState(10);
   const [tuplas, setTuplas] = useState(10);
   const [chave, setChave] = useState('');
-  const { data: resultados, error } = useSWR(['URL_DA_SUA_API', quantidade, chave], fetcher);
+  const [formState, setFormState] = useState(0);
+  const [resultadoDaRequest, setResultadoDaRequest] = useState();
+  const { data: resultados, error } = useSWR(['URL_DA_SUA_API', quantidade, chave, tuplas], fetcher);
 
   //if (error) return <div>Erro ao buscar dados da API</div>;
   //if (!resultados) return <div>Carregando...</div>;
@@ -29,69 +31,130 @@ function App() {
      */
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const response = await fetch('url-do-backend', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({})
-            });
-
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Erro ao fazer requisição: ', error);
+  const fetchData = async () => {
+    try {
+      const response = await axios.post('url-do-backend', {}, {
+        headers: {
+          'Content-Type': 'application/json'
         }
-    };
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Erro ao fazer requisição: ', error);
+    }
+  };
 
+  const onSubmit = async () => {
+    let requestData = {}; 
+  
+    switch (formState) {
+      case 1:
+        requestData = { chave }; 
+        break;
+      case 2:
+        requestData = { quantidade }; 
+        break;
+      case 3:
+        requestData = { tuplas }; 
+        break;
+      default:
+        return;
+    } 
+  
+    try {
+      const response = await axios.post('xxxxx', requestData, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response.data);
+      setResultadoDaRequest(response.data)
+    } catch (error) {
+      console.error('Erro ao fazer requisição: ', error);
+    }
+  };
+
+  const switchButtonClick = () => {
+    //AQUI
+    switch (formState) {
+      case 1:
+        return (
+          <>
+            <div>
+              <p>Chave de busca:</p>
+              <input type="text" value={chave} onChange={(e) => setChave(e.target.value)} />
+            </div>
+            <button type="button" onClick={() => { setFormState(0) }}>Voltar</button>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <div>
+              <p>Número de registros por página:</p>
+              <input type="number" value={quantidade} onChange={(e) => setQuantidade(e.target.value)} />
+            </div>
+            <button type="button" onClick={() => { setFormState(0) }}>Voltar</button>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <div>
+              <p>Número de tuplas para scan:</p>
+              <input type="number" value={tuplas} onChange={(e) => setTuplas(e.target.value)} />
+            </div>
+            <button type="button" onClick={() => { setFormState(0) }}>Voltar</button>
+          </>
+        );
+      case 0:
+        return (
+          <>
+            <button onClick={() => { setFormState(1) }}>Chave de busca</button>
+            <button onClick={() => { setFormState(2) }}>Número de registros por página</button>
+            <button onClick={() => { setFormState(3) }}>Número de tuplas para scan</button>
+          </>
+        );
+    }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <div>
-      <form onSubmit={(e) => { e.preventDefault(); }}>
+      <form onSubmit={(e) => { onSubmit() }}>
         <header>
-            <nav>
-                <h1>#ASH READER</h1>
-            </nav>
+          <nav>
+            <h1>#ASH READER</h1>
+          </nav>
         </header>
         <main className="main">
-            <div className="title">
-                <h2>Campos de busca</h2>
+          <div className="title">
+            <h2>Campos de busca</h2>
+          </div>
+          <section className="pesquisa">
+            <div className="searchArrange">
+              {switchButtonClick()}
             </div>
-            <section className="pesquisa">
-                <div className="searchArrange">
-                    <div>
-                        <p>Chave de busca:</p>
-                        <input type="text" value={chave} onChange={(e) => setChave(e.target.value)}/>
-                    </div>
-                    <div>
-                        <p>Número de registros por página:</p>
-                        <input type="number" value={quantidade} onChange={(e) => setQuantidade(e.target.value)} />
-                    </div>
-                    <div>
-                        <p>Número de tuplas para scan:</p>
-                        <input type="number" value={tuplas} onChange={(e) => setTuplas(e.target.value)} />
-                    </div>
-                </div>
-                <div className="searchArrange">
-                    <button type="submit">Pesquisar</button>
-                </div>
-            </section>
-            <section className="infos">
-                <p>Taxa de colisão:</p>
-                <p>Taxa de overflow:</p>
-                <p>Estimativa de custo:</p>
-            </section>
-            <div className="resultLabel">
-                <h2>Tela de resultados</h2>
-            </div>
-            <section className="dashboard">
-                <Dashboard resultados={resultados} />
-            </section>
+            {formState !== 0 &&
+              (<div className="searchArrange">
+                <button type="submit">Pesquisar</button>
+              </div>)
+            }
+          </section>
+          <section className="infos">
+            <p>Taxa de colisão:</p>
+            <p>Taxa de overflow:</p>
+            <p>Estimativa de custo:</p>
+          </section>
+          <div className="resultLabel">
+            <h2>Tela de resultados</h2>
+          </div>
+          <section className="dashboard">
+            <Dashboard resultados={resultados} />
+          </section>
         </main>
       </form>
     </div>
